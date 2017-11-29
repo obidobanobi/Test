ad29f13171bf074eea81fc9c3f022d3cfc6f40fa
obidobanobi <obidobanobi@gmail.com>
2017-11-29 08:18:59 +0100

    updated...

--- PythonHelloWorld/Checkpoint2/columnMixer.py
+++ PythonHelloWorld/Checkpoint2/columnMixer.py
@@ -1,1 +1,16 @@
+########################################################################################
+# columnMixer.py
+# fancy decription comming soon... =)
+########################################################################################
 
+def mixColumn(col):
+    return 0
+
+def mixColumnInv(col):
+    return 0
+
+def mixColumns(block):
+    return 0
+
+def mixColumnsInv(block):
+    return 0
