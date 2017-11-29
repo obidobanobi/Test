c2d681be9d8af756eec50730356d83ced49cbec7
Leonhard Berg <leonhard.berg@outlook.com>
2017-11-29 10:31:03 +0100

    RowShifter one direction

---
+++ PythonHelloWorld/Checkpoint1/RowShifter.py
@@ -0,0 +1,39 @@
+import os
+from copy import deepcopy   # to copy list by value, not reference
+
+# input block -> output block
+
+
+def shiftRows(block):
+
+    tmp = deepcopy(block)    
+
+	# shift to the left
+	# 0
+	# <
+	# <<
+	# <<<
+
+    tmp[0] = block[0]
+    tmp[1] = block[5]
+    tmp[2] = block[10]
+    tmp[3] = block[15]
+
+    tmp[4] = block[4]
+    tmp[5] = block[9]
+    tmp[6] = block[14]
+    tmp[7] = block[3]
+
+    tmp[8] = block[8]
+    tmp[9] = block[2]
+    tmp[11] = block[7]
+
+    tmp[12] = block[12]
+    tmp[13] = block[1]
+    tmp[14] = block[6]
+    tmp[15] = block[11]
+
+    return tmp;
+
+def shiftRowsInv(block):
+    return block
